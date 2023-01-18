aa5fd04987709ee464aa14b8a716ce745253f310
ludovicjj <jahanlud@gmail.com>
2023-01-18 11:46:06 +0100

    Security: create entity User

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
 ];
