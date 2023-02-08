f750d87cfe33c7e5a57909dc88de68b12769610e
ludovicjj <jahanlud@gmail.com>
2023-02-08 17:17:21 +0100

    Update picture pagination with range

--- assets/js/admin/gallery_update.js
+++ assets/js/admin/gallery_update.js
@@ -24,39 +24,98 @@ new DropFile(
     }
 )
 const pictureContainer = document.querySelector('.picture');
-const total = parseInt(pictureContainer.dataset.total)
-const limit = parseInt(pictureContainer.dataset.limit)
+const paginationContainer = document.querySelector('.pagination');
+
+const totalItems = parseInt(pictureContainer.dataset.total)
+const itemPerPage = parseInt(pictureContainer.dataset.limit)
 const galleryId = pictureContainer.dataset.gallery;
-const currentPage = pictureContainer.dataset.current;
-const page = Math.ceil(total / limit);
-const targetPagination = document.querySelector('.pagination');
 
+const totalPage = Math.ceil(totalItems / itemPerPage);
+
+/**
+ *
+ * @param {number} currentPage
+ * @param {number} totalPage
+ * @return {(number|string)[]}
+ */
+const getPaginationLinks = (currentPage, totalPage) => {
+    const delta = 2;
+    const pages = [...Array(totalPage + 1).keys()].slice(1);
+    const index = pages.findIndex(page => page === currentPage);
+    const start = Math.max(index - delta, 0)
+    const end = Math.min(index + delta + 1, totalPage - 1)
+
+    const limit = (totalPage - delta) -1
+
+    // console.log(`page: ${currentPage}, index: ${index}, start: ${start}, end: ${end}, limit: ${(totalPage - delta) -1}, totalPage: ${totalPage}`)
+
+    if ( limit < delta) {
+        return [...pages.slice(0)];
+    }
+
+    let links
+    if (currentPage < limit) {
+        links = [...pages.slice(start, end), '...', totalPage];
+    } else {
+        links = [...pages.slice(start, end), totalPage];
+    }
+
+    if (links.findIndex(page => page === 2) === -1) {
+        links = ['...', ...links];
+    }
+    if (links.findIndex(page => page === 1) === -1) {
+        links = [1, ...links];
+    }
+    return links;
+}
+
+/**
+ * Build Bootstrap-style pagination links
+ * @param page
+ * @param currentPage
+ * @return {HTMLLIElement}
+ */
+function getPaginationHTML(page, currentPage) {
+    let a = document.createElement('a');
+    a.classList.add('page-link')
+    a.innerText = page;
+    a.setAttribute('href', '#')
+
+    let li = document.createElement('li');
+    li.classList.add('page-item')
+    li.dataset.page = page
+    if (Number.isNaN(Number(page))) {
+        li.classList.add('disabled');
+    }
+
+    if (parseInt(currentPage) === page) {
+        li.classList.add('active')
+    }
+    li.appendChild(a)
+
+    if (!li.classList.contains('disabled')) {
+        li.addEventListener('click', handlePageClick)
+    }
+
+    return li;
+}
 function buildPagination () {
-    let arrayPage = [...Array(page).keys()];
-    arrayPage.forEach(page => {
-        page = (page + 1).toString()
+    const currentPage = pictureContainer.dataset.current;
+    const totalPage = Math.ceil(totalItems / itemPerPage);
 
-        // a
-        let a = document.createElement('a');
-        a.classList.add('page-link')
-        a.innerText = page;
-        a.setAttribute('href', '#')
+    let paginationLinks = getPaginationLinks(parseInt(currentPage), totalPage);
 
-        // li
-        let li = document.createElement('li');
-        li.classList.add('page-item')
-        li.dataset.page = page
-        if (currentPage === page) {
-            li.classList.add('active')
-        }
-
-        // add event
-        li.addEventListener('click', handlePage)
-
-        // append
-        li.appendChild(a)
-        targetPagination.appendChild(li);
+    paginationLinks.forEach(link => {
+        const linkHTML = getPaginationHTML(link, currentPage)
+        paginationContainer.appendChild(linkHTML)
     })
+}
+const handlePageClick = (e) => {
+    e.preventDefault();
+    pictureContainer.dataset.current = e.currentTarget.dataset.page;
+    paginationContainer.replaceChildren();
+
+    buildPagination()
 }
 const handlePage = async (e) => {
     e.preventDefault();
@@ -91,4 +150,4 @@ const handlePage = async (e) => {
 
     }
 }
-buildPagination();
+buildPagination(totalPage);
