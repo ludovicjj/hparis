c1c203c8e5e827411484953c44c1b4504ad62c6e
ludovicjj <jahanlud@gmail.com>
2023-02-14 16:29:56 +0100

    Handle form gallery update

--- src/Controller/Admin/AdminGalleryController.php
+++ src/Controller/Admin/AdminGalleryController.php
@@ -4,13 +4,14 @@ namespace App\Controller\Admin;
 
 use App\Builder\ErrorsValidationBuilder;
 use App\Entity\Gallery;
-use App\Form\Handler\CreateGalleryHandler;
+use App\Form\Handler\GalleryHandler;
 use App\Form\Type\GalleryType;
 use App\Repository\GalleryRepository;
 use App\Repository\PictureRepository;
 use App\Service\PictureCleaner;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -31,11 +32,11 @@ class AdminGalleryController extends AbstractController
 
     #[Route('/gallery/create', name: 'gallery_create')]
     public function create(
-        Request $request,
-        CreateGalleryHandler $galleryHandler,
-        ValidatorInterface $validator,
+        Request                $request,
+        GalleryHandler         $galleryHandler,
+        ValidatorInterface     $validator,
         EntityManagerInterface $entityManager,
-        PictureCleaner $pictureCleaner
+        PictureCleaner         $pictureCleaner
     ): Response
     {
         $form = $this->createForm(GalleryType::class, new Gallery(), [
@@ -43,7 +44,7 @@ class AdminGalleryController extends AbstractController
         ]);
 
         if ($request->isXmlHttpRequest() && $request->isMethod('POST')) {
-            $gallery = $galleryHandler->handle($request);
+            $gallery = $galleryHandler->handle($request, $form);
             $constraintList = $validator->validate($gallery);
             ErrorsValidationBuilder::buildErrors($constraintList);
 
@@ -62,10 +63,13 @@ class AdminGalleryController extends AbstractController
 
     #[Route('/gallery/update/{id}', name: 'gallery_update')]
     public function update(
-        Request $request,
-        int $id,
-        GalleryRepository $galleryRepository,
-        PictureRepository $pictureRepository
+        Request            $request,
+        int                $id,
+        GalleryRepository  $galleryRepository,
+        PictureRepository  $pictureRepository,
+        GalleryHandler     $galleryHandler,
+        ValidatorInterface $validator,
+        EntityManagerInterface $entityManager,
     ): Response
     {
         $gallery = $galleryRepository->findGalleryUpdate($id);
@@ -74,20 +78,33 @@ class AdminGalleryController extends AbstractController
             throw new NotFoundHttpException('gallery not found');
         }
 
-        $paginatedPicture = $pictureRepository->paginatedPictureByGallery($id, 1);
-        $pictures = iterator_to_array($paginatedPicture);
-
+        $gallery->getThumbnail()->setImageFile(
+            new File($this->getParameter('thumbnail_directory') .'/'.$gallery->getThumbnail()->getImageName())
+        );
 
         $form = $this->createForm(GalleryType::class, $gallery, [
             'action' => $this->generateUrl('admin_gallery_update', ['id' => $id])
         ]);
 
+        if ($request->isXmlHttpRequest() && $request->isMethod('POST')) {
+            $gallery = $galleryHandler->handle($request, $form);
+            $constraintList = $validator->validate($gallery);
+            ErrorsValidationBuilder::buildErrors($constraintList);
+
+            $entityManager->flush();
+
+            return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+        }
+
+        $paginatedPicture = $pictureRepository->paginatedPictureByGallery($id, 1);
+        $pictures = iterator_to_array($paginatedPicture);
+
         return $this->render('/admin/gallery_update.html.twig', [
-            'form' => $form,
-            'pictures' => $pictures,
+            'form'          => $form,
+            'pictures'      => $pictures,
             'totalPictures' => $paginatedPicture->count(),
-            'per_page' => PictureRepository::ITEMS_PER_PAGE,
-            'galleryId' => $id
+            'per_page'      => PictureRepository::ITEMS_PER_PAGE,
+            'galleryId'     => $id
         ]);
     }
 }
