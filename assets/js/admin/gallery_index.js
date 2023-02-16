5d79a66e93b4faeb15f526f1886cdaec8303bd99
ludovicjj <jahanlud@gmail.com>
2023-02-16 16:55:09 +0100

    Enable bootstrap tooltip

--- assets/js/admin/gallery_index.js
+++ assets/js/admin/gallery_index.js
@@ -1,1 +1,12 @@
 import '../../styles/admin/gallery_index.scss';
+import { Tooltip } from 'bootstrap';
+import PaginatedGallery from "../components/PaginatedGallery";
+
+document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach(tooltipTriggerEl => {
+    new Tooltip(tooltipTriggerEl)
+})
+
+new PaginatedGallery(
+    document.querySelector('.gallery'),
+    document.querySelector('.pagination')
+);
