f4e896017079e9025c7fdbc166b6b34e04a7fc25
ludovicjj <jahanlud@gmail.com>
2023-02-22 16:37:18 +0100

    InfiniteScroll: Add loader. Add button to come back top page

--- assets/js/admin/gallery_read.js
+++ assets/js/admin/gallery_read.js
@@ -1,68 +1,18 @@
 import '../../styles/admin/gallery_read.scss';
+import {InfinitePictureScroll} from "../components/InfinitePictureScroll";
 
-const pictureContainer = document.querySelector('.picture-container')
-const watcher = document.querySelector('.intersection-watcher')
 
-const count = 12
-const galleryId = pictureContainer?.dataset.gallery
-let page = parseInt(pictureContainer?.dataset.page)
-const url = pictureContainer?.dataset.url
+const top = document.getElementById('top');
 
-const fetchData = async (url) => {
-    const response = await fetch(url, {
-        method: 'GET',
-        headers: {
-            'X-Requested-With': 'XMLHttpRequest',
-            'Accept': 'application/json'
-        }
-    });
-
-    return (
-        response.headers.has('content-type') && response.headers.get('content-type') === 'application/json'
-    ) ? await response.json() : null
+if (top) {
+    top.addEventListener('click', () => {
+        document.body.scrollTop = 0
+        document.documentElement.scrollTop = 0
+    })
 }
 
-const createPicture = (imageName) => {
-    const div = document.createElement('div');
-    div.classList.add('col-6', 'col-sm-4', 'col-md-3', 'mb-4')
-
-    const card = document.createElement('div')
-    card.classList.add('card', 'gallery-card')
-
-    const picture = document.createElement('img')
-    picture.className = 'picture'
-    picture.setAttribute('alt', 'Gallery Picture')
-    picture.setAttribute('src', `/uploads/pictures/${imageName}`)
-
-    card.appendChild(picture)
-    div.appendChild(card)
-    pictureContainer.querySelector('.row').appendChild(div)
-
-}
-
-const addContent = async () => {
-    page = page + 1
-
-    const targetUrl = url + `?id=${encodeURIComponent(galleryId)}&page=${encodeURIComponent(page)}&count=${count}`
-    const data = await fetchData(targetUrl)
-
-    if (data) {
-        data.forEach(({imageName}) => {
-            createPicture(imageName)
-        })
-        if (!data.length || data.length !== 12) {
-            watcher.remove()
-        }
-    }
-}
-
-const handleIntersect = entries => {
-    if (entries[0].isIntersecting) {
-        addContent().then(_ => {
-            pictureContainer.dataset.page = page
-        })
-    }
-}
-if (pictureContainer && watcher) {
-    new IntersectionObserver(handleIntersect).observe(watcher)
-}
+new InfinitePictureScroll(
+    12,
+    '.intersection-watcher',
+    '.picture-container'
+)
---
+++ assets/js/components/InfinitePictureScroll.js
@@ -0,0 +1,78 @@
+export class InfinitePictureScroll {
+    /**
+     *
+     * @param {number} perPage
+     * @param {string} watcherClassName
+     * @param {string} containerClassName
+     */
+    constructor(perPage, watcherClassName, containerClassName) {
+        this.perPage = perPage
+        this.watcher = document.querySelector(watcherClassName)
+        this.container = document.querySelector(containerClassName)
+
+        if (!this.watcher || !this.container) {
+            return
+        }
+        this.galleryId = this.container.dataset.gallery
+        this.page = parseInt(this.container.dataset.page)
+        this.url = this.container.dataset.url
+
+        new IntersectionObserver(this.handleIntersect.bind(this)).observe(this.watcher)
+    }
+
+    handleIntersect(entries) {
+        if (entries[0].isIntersecting) {
+            this.page++
+            this.watcher.classList.add('loading')
+            this.addContent().then(_ => {
+                this.watcher.classList.remove('loading')
+            })
+        }
+    }
+
+    async addContent() {
+        const url = this.url + `?id=${encodeURIComponent(this.galleryId)}&page=${encodeURIComponent(this.page)}&count=${this.perPage}`
+        const data = await this.fetchData(url);
+
+        if (data) {
+            data.forEach(({imageName}) => {
+                this.createPicture(imageName)
+            })
+            if (!data.length || data.length !== this.perPage) {
+                this.watcher.remove()
+            }
+        }
+    }
+
+    async fetchData(url) {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'X-Requested-With': 'XMLHttpRequest',
+                'Accept': 'application/json'
+            }
+        });
+
+        return (
+            response.headers.has('content-type') &&
+            response.headers.get('content-type') === 'application/json'
+        ) ? await response.json() : null
+    }
+
+    createPicture(imageName) {
+        const div = document.createElement('div');
+        div.classList.add('col-6', 'col-sm-4', 'col-md-3', 'mb-4')
+
+        const card = document.createElement('div')
+        card.classList.add('card', 'gallery-card')
+
+        const picture = document.createElement('img')
+        picture.className = 'picture'
+        picture.setAttribute('alt', 'Gallery Picture')
+        picture.setAttribute('src', `/uploads/pictures/${imageName}`)
+
+        card.appendChild(picture)
+        div.appendChild(card)
+        this.container.querySelector('.row').appendChild(div)
+    }
+}
