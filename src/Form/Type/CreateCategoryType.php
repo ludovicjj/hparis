941e1224e64d6a7c44404e55b0daed5c446ba77d
ludovicjj <jahanlud@gmail.com>
2023-03-02 16:59:10 +0100

    Create category form

---
+++ src/Form/Type/CreateCategoryType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form\Type;
+
+use App\Entity\Category;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CreateCategoryType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+        ->add('name', TextType::class, [
+            'label' => 'form.category.name.label',
+            'help' => 'form.category.name.help'
+        ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Category::class,
+            'translation_domain' => 'form'
+        ]);
+    }
+}
